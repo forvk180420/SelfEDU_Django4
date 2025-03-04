ac3f12f6d829226831e039e8ef96c4c4f8150a20
Simon <forvk180420@gmail.com>
2025-03-04 22:59:25 +0300

    2,3

---
+++ 2. Установка Django. Создание проекта.py
@@ -0,0 +1,22 @@
+"""
+После создания виртуального окружения обновляем pip:
+python.exe -m pip install --upgrade pip
+
+Устанавливаем Django:
+pip install django
+
+Проверяем список установленных пакетов:
+pip list
+
+Заходим в админ команды:
+django-admin
+
+Переходим в корневой каталог и формируем проект:
+django-admin startproject mysitedjango
+
+Переходим в каталог с названием сайта (mysitedjango) и запускаем тестовый сервер:
+cd mysitedjango
+python manage.py runserver
+или
+python manage.py runserver 4000 - если нужно запустить сервер на определенном порту
+"""
