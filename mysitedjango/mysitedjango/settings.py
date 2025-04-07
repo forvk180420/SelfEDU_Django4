3f16aabf25f3b1ea17f71976557a4106622a5d3e
Simon <forvk180420@gmail.com>
2025-04-07 14:32:01 +0300

    15+, 16

--- mysitedjango/mysitedjango/settings.py
+++ mysitedjango/mysitedjango/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-747^e_v49by@xl!48cl8ji*yudfnmxx%qazm-1&+cbr_(51akc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
